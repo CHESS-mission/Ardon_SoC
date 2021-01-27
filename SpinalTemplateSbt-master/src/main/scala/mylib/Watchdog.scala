1178f68bd995666a9d7b7c1b607ffdf9b6994891
Michael Linder <40787689+Mlx11@users.noreply.github.com>
2021-01-27 16:02:56 +0100

    small stuff

--- SpinalTemplateSbt-master/src/main/scala/mylib/Watchdog.scala
+++ SpinalTemplateSbt-master/src/main/scala/mylib/Watchdog.scala
@@ -5,27 +5,6 @@ import spinal.core._
 import spinal.lib._
 import spinal.lib.fsm._
 
-// APB definition
-
-case class ApbConfig(addressWidth : Int,
-                     dataWidth    : Int,
-                     selWidth     : Int)
-
-case class Apb(config: ApbConfig) extends Bundle with IMasterSlave {
-  val PSEL       = Bits(config.selWidth bits)
-  val PENABLE    = Bool
-  val PADDR      = UInt(config.addressWidth bit)
-  val PWRITE     = Bool
-  val PWDATA     = Bits(config.dataWidth bit)
-  val PRDATA     = Bits(config.dataWidth bit)
-  val PREADY     = Bool
-
-  override def asMaster(): Unit = {
-    out(PADDR,PSEL,PENABLE,PWRITE,PWDATA)
-    in(PREADY,PRDATA)
-  }
-}
-
 // Watchdog component
 
 class Watchdog(apbConfig: ApbConfig, counter_trigger_value: Int, counter_width: Int, PSEL_Nr: Int) extends Component {
